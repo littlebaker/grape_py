716e10116dfbd47542b43aed695bdb322a1edc4d
lishengyong <bakerhello@163.com>
2023-09-04 13:58:41 +0800

    transform from Qobj to ndarray

--- test.py
+++ test.py
@@ -10,17 +10,17 @@ def test_grape():
     
     # desired state is (|0> + exp(-i*pi/4)|1>)/sqrt(2)
     state_T = (basis(2, 0) + np.exp(-1j * np.pi / 4) * basis(2, 1)) / np.sqrt(2)
-    C = ket2dm(state_T)
+    C = ket2dm(state_T).full()
     
-    H0 = sm.dag() * sm
+    H0 = (sm.dag() * sm).full()
     # H1 is sigma_x
-    H1 = sm + sm.dag()
+    H1 = (sm + sm.dag()).full()
     # H2 is sigma_y
-    H2 = -1j * (sm - sm.dag())
+    H2 = (-1j * (sm - sm.dag())).full()
     
     Hk = [H1, H2]
     T = 1
-    threshold, u_kj, rho_T = grape(H0, Hk, np.zeros((2, 50)), rho_0, C, T)
+    threshold, u_kj, rho_T = grape(H0, Hk, np.zeros((2, 50)), rho_0.full(), C, T)
     
     print(u_kj)
     assert np.allclose(rho_T, C, atol=1e-2) == True, "final state is not close to target state"
